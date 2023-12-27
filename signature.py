7964405299653e9dded3d04e5ae870ebe91cdfb8
mralinp <alinaderiparizi@yahoo.com>
2023-12-27 15:54:49 +0330

    changed tdsc dataset implementation

--- signature.py
+++ signature.py
@@ -20,14 +20,16 @@ def signature(binary_image, res=2):
     boundary = get_boundary(binary_image)
     cords_x, cords_y = np.nonzero(boundary)
     boundary_cords = list(zip(cords_x, cords_y))
-    print(boundary_cords)
     signature = np.zeros([360//res], dtype=np.float32)
     for theta in range(0, 360, res):
-        slop = np.tan(theta)
+        slop = np.tan(np.radians(theta))
         # m.x + c = y => c = tan(theta).x0-y0
-        c = slop*x0-y0
-        for x,y in boundary_cords:
+        c = -1*slop*x0+y0
+        dif_min = 10000
+        for x, y in boundary_cords:
             y_pred = slop*x + c
-            if y_pred - y < 0.01:
+            dif = np.abs(y_pred - y)
+            if(dif < dif_min):
+                dif_min = dif
                 signature[theta//res] = np.sqrt((x0-x)**2 + (y0-y)**2)
     return signature
