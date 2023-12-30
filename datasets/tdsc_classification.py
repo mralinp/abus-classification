6938220853de350961184ebfd9117cd64b22c38e
mralinp <alinaderiparizi@yahoo.com>
2023-12-30 17:23:52 +0330

    changed classification dataset

--- datasets/tdsc_classification.py
+++ datasets/tdsc_classification.py
@@ -2,48 +2,19 @@ import os
 import torch
 from PIL import Image
 import numpy as np
-import random
 
 
-train_list = None
-validation_list = None
-
-
-def split_data(m, b):
-
-    global train_list, validation_list
-
-    m_t = set(random.sample(m, int(0.8*len(m))))
-    b_t = set(random.sample(b, int(0.8*len(b))))
-    m = set(m)
-    b = set(b)
-    m_v = m-m_t
-    b_v = b-b_t
-    train_list = list(m_t) + list(b_t)
-    validation_list = list(m_v) + list(b_v)
-    random.shuffle(train_list)
-    random.shuffle(validation_list)
-
-
-class TDSC2D(torch.utils.data.Dataset):
+class TDSCForClassification(torch.utils.data.Dataset):
     
-    def __init__(self, path="./data/tdsc/slices", train=True, transforms=None):
+    def __init__(self, path="./data/tdsc/classification", transforms=None):
 
-        global train_list, validation_list
         self.path = path
         self.transforms = transforms
         malignant = os.listdir(f"{path}/data/0")
         benign = os.listdir(f"{path}/data/1")
         malignant = [(f"data/0/{item}", 0) for item in malignant]
         benign = [(f"data/1/{item}", 1) for item in benign]
-
-        if not train_list or not validation_list:
-            split_data(malignant, benign)
-
-        if train:
-            self.data_list = train_list
-        else:
-            self.data_list = validation_list
+        self.data_list = malignant + benign
             
     def __len__(self):
         return len(self.data_list)
@@ -61,7 +32,7 @@ class TDSC2D(torch.utils.data.Dataset):
         return image, mask, float(label)
 
 if __name__ == "__main__":
-    dataset = TDSC2D(path="../data/tdsc/slices", train = True)
+    dataset = TDSCForClassification(path="../data/tdsc/slices")
     print(len(dataset))
     sample = dataset[0]
         
