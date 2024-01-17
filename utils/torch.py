f2d153e5885d652c3e5e2d5d3276d1f6a51dc89a
mralinp <alinaderiparizi@yahoo.com>
2024-01-17 12:17:01 +0330

    subsampling implemented

--- utils/torch.py
+++ utils/torch.py
@@ -9,12 +9,10 @@ def calculate_accuracy(dataset, model, device="cuda"):
 
     with torch.no_grad():
         for data in dataset:
-            x, m, y = data
+            x, y = data
             x = x.to(device)
-            m = m.unsqueeze(1).to(device)
             y = y.unsqueeze(1).to(device)
-            x = x - x * m * 0.5
-
+            
             predictions = torch.sigmoid(model(x))
             predictions = (predictions > 0.5).float()
             num_correct += (predictions == y).sum()
@@ -39,12 +37,3 @@ def save_predictions_as_imgs(loader, model, folder="saved_images/", device="cuda
         torchvision.utils.save_image(y.unsqueeze(1), f"{folder}{idx}.png")
 
     model.train()
-    
-def zero_pad_resize(img, size=(224,224)):
-    res = np.zeros(size, dtype=np.float32)
-    cx, cy = size[0]//2, size[1]//2
-    w,h = img.shape
-    if w > size[0] or h > size[1]:
-        raise f"Cant resize with zero padding from origin with shape {img.shape} to size {size}"
-    res[cx-w//2:cx+w//2, cy-h//2:cy+h//2] = img
-    return res
