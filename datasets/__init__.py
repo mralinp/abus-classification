327911257ec5c72e019bde699617d405f0ecb637
mralinp <alinaderiparizi@yahoo.com>
2024-02-08 11:35:53 +0330

    leave one out with 81% of accuracy

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,6 +4,7 @@ from .tdsc_classification import TDSCForClassification
 from .tdsc_detection import TDSCForDetection
 from .tdsc_subsampling import TDSCForClassificationWithSub
 from .tdsc_tumors import TDSCTumors
+from .tumors import Tumors
 
 
 __all__ = [
