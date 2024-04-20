ac3f66c01e9f322fe9dc294af53ed547037d5beb
mralinp <alinaderiparizi@yahoo.com>
2024-04-20 23:08:00 +0330

    fixed boundary signature 2d

--- abus_classification/utils/features/signature.py
+++ abus_classification/utils/features/signature.py
@@ -2,23 +2,30 @@ import cv2
 import numpy as np
 from .. import image
 
-def signature(binary_image, res=1):
-    binary_image = image.rotation_invariant(binary_image)
-    x0, y0 = image.find_shape_center(binary_image)
-    c = x0 - y0
-    boundary = image.get_boundary(binary_image)
-    cords_x, cords_y = np.nonzero(boundary)
+def boundary_signature_2d(boundary_image: np, resolution:float=1., dif_min:int=5) -> np:
+    
+    assert dif_min >= 0
+    assert resolution >= 0
+    
+    x0, y0 = image.find_shape_center(boundary_image)
+    cords_x, cords_y = np.nonzero(boundary_image)
     boundary_cords = list(zip(cords_x, cords_y))
-    signature = np.zeros([360//res], dtype=np.float32)
-    for theta in range(0, 360, res):
+    signature = np.zeros([int(360/resolution)], dtype=np.float32)
+    len_signature = signature.shape[0]
+
+    for idx in range(len_signature):
+        # m*x + c = y => c = -tan(theta)*x0 + y0
+        theta = idx*resolution
         slop = np.tan(np.radians(theta))
-        # m.x + c = y => c = tan(theta).x0-y0
+        if theta == 90 or theta == 270:
+            slop = 0
         c = -1*slop*x0+y0
-        dif_min = 10000
+        best_dif = 1000
         for x, y in boundary_cords:
             y_pred = slop*x + c
             dif = np.abs(y_pred - y)
-            if(dif < dif_min):
-                dif_min = dif
-                signature[theta//res] = np.sqrt((x0-x)**2 + (y0-y)**2)
+            if dif < dif_min and dif < best_dif:
+                best_dif = dif
+                signature[idx] = np.sqrt((x0-x)**2 + (y0-y)**2)
+
     return signature
--- test/utils_tests/features_tests/test_signature.py
+++ test/utils_tests/features_tests/test_signature.py
@@ -2,20 +2,18 @@ import numpy as np
 from abus_classification import utils
 
 
-def test_signature():
+def test_boundary_signature_2d():
     
-    x = np.array([[0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0],
-                  [0,0,1,1,1,1,1,1,0,0],
-                  [0,0,1,0,0,0,0,1,0,0],
-                  [0,0,1,0,0,0,0,1,0,0],
-                  [0,0,1,0,0,0,0,1,0,0],
-                  [0,0,1,0,0,0,0,1,0,0],
-                  [0,0,1,1,1,1,1,1,0,0],
-                  [0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0]], dtype=np.uint8) * 255
+    x = np.array([[1,1,1,1,1],
+                  [1,0,0,0,1],
+                  [1,0,0,0,1],
+                  [1,0,0,0,1],
+                  [1,1,1,1,1]], dtype=np.uint8) * 255
+    y = np.array([[2.,2.828427,2.,2.828427,2.,2.828427,2.,2.828427]], dtype=np.float32)
     
-    sig = utils.features.signature(x, res=1)
+    sig = utils.features.boundary_signature_2d(x, resolution=45)
     
     print(sig)
+    
+    assert np.equal(sig,y).all()
     
