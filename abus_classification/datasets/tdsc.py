cdbc7080ab3c60cb73bc27b1ad4804681804d531
mralinp <alinaderiparizi@yahoo.com>
2024-04-22 14:52:48 +0330

    changed download method in tdsc

--- abus_classification/datasets/tdsc.py
+++ abus_classification/datasets/tdsc.py
@@ -1,6 +1,5 @@
 import os
 import nrrd
-import torch
 import pandas as pd
 from typing import Final
 
@@ -9,11 +8,29 @@ from .google_drive_downloader import GoogleDriveDownloader
 
 
 DATASET_IDS: Final = {
-    "data_0": "1K762mw8vAIRjgoeR7aJN-0NIntOBUb6O",
-    "data_1": "17Umzl10lpFu4mGJ9HrZrC-Lcc-klKcl1",
-    "mask": "1Z2RUoUoOukA93LyTgwKPKCrfFePva1pV",
-    "labels": "1Fn6psOjknovxmShESRYpaxDAbb9txvH7",
-    "bbx_labels": "1firgUGMMMscXoYlQCzdg8Y7x2Hc3enIt"
+    "data_0.zip": {
+        "id":"1K762mw8vAIRjgoeR7aJN-0NIntOBUb6O",
+        "path": "DATA",
+        "zip": True
+    },
+    "data_1.zip": {
+        "id":"17Umzl10lpFu4mGJ9HrZrC-Lcc-klKcl1",
+        "path": "DATA",
+        "zip": True
+    },
+    "mask.zip": {
+        "id":"1Z2RUoUoOukA93LyTgwKPKCrfFePva1pV",
+        "path": "MASK",
+        "zip": True
+    },
+    "labels.csv": {
+        "id":"1Fn6psOjknovxmShESRYpaxDAbb9txvH7",
+        "path": "."
+    },
+    "bbx_labels.csv": {
+        "id":"1firgUGMMMscXoYlQCzdg8Y7x2Hc3enIt",
+        "path": "."
+    }
 }
 
 
@@ -35,16 +52,10 @@ class TDSC(Dataset):
     
     def download_data(self):
         downloader = GoogleDriveDownloader(None)
-        downloader.download(DATASET_IDS.get("data_0"), f"{self.path}/DATA/data0.zip")
-        downloader.save()
-        downloader.download(DATASET_IDS.get("data_1"), f"{self.path}/DATA/data1.zip")
-        downloader.save()
-        downloader.download(DATASET_IDS.get("mask"), f"{self.path}/MASK/mask.zip")
-        downloader.save()
-        downloader.download(DATASET_IDS.get("labels"), f"{self.path}/labels.csv")
-        downloader.download(DATASET_IDS.get("bbx_labels"), f"{self.path}/bbx_labels.csv")
-        
-        
+        for file_name, file_info in DATASET_IDS.items:
+            downloader.download(file_info.get("id"), f"{self.path}/{file_info.get("path")}/{file_name}")
+            if file_info.get("zip"):
+                downloader.save()
         
     def __getitem__(self, index) -> tuple:
         label, vol_path, mask_path = self.metadata.iloc[index]
