4ceff3a9f10e67e819cce5d27e3f785cf8622d3b
mralinp <alinaderiparizi@yahoo.com>
2024-05-04 00:32:11 +0330

    completed vnet

--- test/models_tests/test_vnet.py
+++ test/models_tests/test_vnet.py
@@ -1,11 +1,10 @@
 import torch
-# from abus_classification.models import VNet
+from abus_classification.models import VNet
 
 def test_vnet():
-    pass
-    # # Example usage
-    # model = VNet(in_channels=64, num_classes=2)
-    # # Assuming input shape is [batch_size, channels, depth, height, width]
-    # input_tensor = torch.randn(1, 1, 64, 64, 64)
-    # output_tensor = model(input_tensor)
-    # print(output_tensor.shape)  # Print the shape of the output tensor
+    
+    model = VNet(in_channels=1, out_channels=1)
+    # Assuming input shape is [batch_size, channels, depth, height, width]
+    input_tensor = torch.randn(1, 1, 128, 128, 64)
+    output_tensor = model(input_tensor)
+    print(output_tensor.shape)  # Print the shape of the output tensor
